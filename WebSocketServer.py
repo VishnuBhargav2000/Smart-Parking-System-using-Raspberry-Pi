b1f2f69b01fa2a1fa0797dbb5d1e5393edc2b384
VishnuBhargav2000 <40735102+VishnuBhargav2000@users.noreply.github.com>
2021-03-13 17:35:11 +0530

    some minor changes
    done some minor changes to the websocket server code

--- WebSocketServer.py
+++ WebSocketServer.py
@@ -1,53 +1,54 @@
-#importing dependencies
+# importing dependencies
 import RPi.GPIO as GPIO
 import time
 import asyncio
 import websockets
 
-#setting pins for pi 
-TRIG=7
-ECHO=11
+# setting pins for pi
+TRIG = 7
+ECHO = 11
 GPIO.setmode(GPIO.BOARD)
 
 
 def GetInfo():
     # driver code for ultrasonic sensor thats connected to raspberry pi
     # returns a string (True/False) based on the distance data accquired from the sensor
-    # True = if distance greater than a pre defined value, False if lesser. which indicates if a spot is taken or available 
+    # True = if distance greater than a pre defined value, False if lesser. which indicates if a spot is taken or available
     isThere = True
-    GPIO.setup(TRIG,GPIO.OUT)
-    GPIO.setup(ECHO,GPIO.IN)
-    
-    GPIO.output(TRIG,False)
+    GPIO.setup(TRIG, GPIO.OUT)
+    GPIO.setup(ECHO, GPIO.IN)
+
+    GPIO.output(TRIG, False)
     time.sleep(0.2)
-    
-    GPIO.output(TRIG,True)
+
+    GPIO.output(TRIG, True)
     time.sleep(0.00001)
-    GPIO.output(TRIG,False)
-    
-    while GPIO.input(ECHO)==0:
-        pulse_start=time.time()
-    while GPIO.input(ECHO)==1:
-        pulse_end=time.time()
-    
-    pulse_duration=pulse_end-pulse_start
-    distance=pulse_duration*17150
-    distance=round(distance,2)
+    GPIO.output(TRIG, False)
+
+    while GPIO.input(ECHO) == 0:
+        pulse_start = time.time()
+    while GPIO.input(ECHO) == 1:
+        pulse_end = time.time()
+
+    pulse_duration = pulse_end - pulse_start
+    distance = pulse_duration * 17150
+    distance = round(distance, 2)
     if distance > 10:
         isThere = False
     else:
         isThere = True
-    return str(isThere)    
+    return str(isThere)
 
 
 async def webserver(websocket, path):
     # sends info on the parking spots to all connected clients every half-second
     while True:
-        await websocket.send(GetInfo()) # waits for new connections then sends info to all connected clients
-        await asyncio.sleep(0.5) 
+        await websocket.send(GetInfo())  # waits for new connections then sends info to all connected clients
+        await asyncio.sleep(0.5)
 
-# driver code for websocket server
-start_server = websockets.serve(webserver, "192.168.0.8", 5678) 
+    # driver code for websocket server
+
+
+start_server = websockets.serve(webserver, "192.168.0.8", 5678)
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
-
