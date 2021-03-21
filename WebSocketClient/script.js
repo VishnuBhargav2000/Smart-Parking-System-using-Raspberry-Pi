aa57fd6a60be95889be41b37d217885cb864d132
VishnuBhargav2000 <40735102+VishnuBhargav2000@users.noreply.github.com>
2021-03-21 13:36:52 +0530

    ADDED ability to add multiple sensors

--- WebSocketClient/script.js
+++ WebSocketClient/script.js
@@ -1,12 +1,18 @@
 // basic websocket client webpage made as a proof of concept
-var ws = new WebSocket("ws://192.168.0.10:5678/"); // connects to websocket server            
+var ws = new WebSocket("ws://IP:PORT/"); // connects to websocket server            
 ws.onmessage = function (event) {
                     // when server sends a message this block executes
     
-    if (event.data == "True") {                
+    if (event.data[0] == 'T') {                
         document.getElementById("spot1").style.backgroundColor = "green";}
 
     else{
         document.getElementById("spot1").style.backgroundColor = "red";}
+
+    if (event.data[1] == 'T') {                
+        document.getElementById("spot2").style.backgroundColor = "green";}
+
+    else{
+        document.getElementById("spot2").style.backgroundColor = "red";}
                     
     };
--- WebSocketServer/WebSocketServer.py
+++ WebSocketServer/WebSocketServer.py
@@ -4,18 +4,20 @@ import time
 import asyncio
 import websockets
 
-# setting pins for pi
-    TRIG = 7
-    ECHO = 11
-    GPIO.setmode(GPIO.BOARD)
 
-def GetInfo():
+# setting pins for pi
+triggers = [pin1,pin2]  # Trigger pins (viable pins - [7,13])
+echos = [pin1,pin2]  # Echo pins  (viable pins - [11, 24]) 
+GPIO.setmode(GPIO.BOARD) 
+GPIO.setwarnings(False)
+
+
+def GetDistance(TRIG,ECHO, carDistance):
     # driver code for ultrasonic sensor thats connected to raspberry pi
     # returns a string (True/False) based on the distance data accquired from the sensor
     # True = if distance greater than a pre defined value, False if lesser. which indicates if a spot is taken or available
-    
-    isThere = True
 
+    isThere = 'T'
     GPIO.setup(TRIG, GPIO.OUT)
     GPIO.setup(ECHO, GPIO.IN)
 
@@ -34,11 +36,22 @@ def GetInfo():
     pulse_duration = pulse_end - pulse_start
     distance = pulse_duration * 17150
     distance = round(distance, 2)
-    if distance > 10:
-        isThere = False
+
+    if distance > carDistance:
+
+        isThere = 'F'
     else:
-        isThere = True
-    return str(isThere)
+        isThere = 'T'
+
+    return isThere
+
+def GetInfo():
+    NumberOfSensors = 2 # number of Hc-SR04 modules used
+    carDistance = 5 # distance between Hc-SC04 module and a car parked in the respective parking lot
+    info = ''
+    for sensor in range(NumberOfSensors):
+        info += GetDistance(triggers[sensor],echos[sensor],carDistance)
+    return info
 
 
 async def webserver(websocket, path):
@@ -48,6 +61,8 @@ async def webserver(websocket, path):
         await asyncio.sleep(0.5)
 
 # driver code for websocket server
-start_server = websockets.serve(webserver, "IP", HOST)
+start_server = websockets.serve(webserver, "IP", PORT)
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
+
+
